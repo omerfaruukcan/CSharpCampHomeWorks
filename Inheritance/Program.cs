2567b4d96f7398a0970e14a75224f788af203365
omerfaruukcan <ofarukcan@trakya.edu.tr>
2022-04-24 12:25:28 +0300

    homework

---
+++ Inheritance/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person[] persons = new Person[3] 
+            {
+                new Person { FirstName = "Busra" },
+                new Customer { FirstName = "Omer Faruk" },
+                new Student() {FirstName = "Hatice" }
+            };
+
+            foreach (var person in persons)
+            {
+                Console.WriteLine(person.FirstName);
+            }
+        }
+    }
+
+    class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+    interface IPerson
+    {
+
+    }
+
+    class Customer : Person, IPerson
+    {
+        public string City { get; set; }
+    }
+
+    class Student : Person
+    {
+        public string Departmant { get; set; }
+    }
+}
+
+// Bir class sadece bir classi miras alabilir 2 tane alamaz fakat birden cok interfacesi alabilir.
+// önce inheritance lar yazilir sonra interfaceler yazilir.
+// class larin tek basina bi anlami vardir fakat interfacelerin yoktur.
+
+// abstract classlarda inheritence kullanilir genelde, eger interface kullanilabiliyorsa genelde interface kullanilir
+
---
+++ Interfaces/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace Interfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PersonManager personManager = new PersonManager();
+            Customer customer = new Customer() 
+            {
+                Id = 1, 
+                FirstName = "Omer Faruk",
+                LastName = "CAN", 
+                Address = "Istanbul"
+            };
+
+            Student student = new Student()
+            {
+                Id = 1,
+                FirstName = "Busra",
+                LastName = "CAN",
+                Departmant = "Health "
+            };
+
+            personManager.Add(customer);
+            personManager.Add(student);
+
+            Console.WriteLine("----------------------------");
+
+            CustomerManager customerManager = new CustomerManager();
+            SqlServerCustomerDal sqlServerCustomerDal = new SqlServerCustomerDal();
+
+            customerManager.Add(sqlServerCustomerDal);
+            customerManager.Add(new OracleCustomerDal());
+
+            Console.WriteLine("----------------------------");
+
+            ICustomerDal[] customerDals = new ICustomerDal[3]
+            {
+                new SqlServerCustomerDal(),
+                new OracleCustomerDal(),
+                new MySqlCustomerDal()
+            };
+
+            foreach (var customerDal in customerDals)
+            {
+                customerDal.Add();
+            }
+        }
+
+        interface IPerson
+        {
+            int Id { get; set; }
+            string FirstName { get; set; }
+            string LastName { get; set; }
+        }
+
+        class Customer : IPerson
+        {
+            public int Id { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string Address { get; set; }
+        }
+
+        class Student : IPerson
+        {
+            public int Id { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string Departmant { get; set; }
+        }
+
+        class PersonManager
+        {
+            public void Add(IPerson person)
+            {
+                Console.WriteLine(person.FirstName);
+            }
+        }
+    }
+}
